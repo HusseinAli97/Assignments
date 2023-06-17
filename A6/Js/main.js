8f0c64d5e456c403ea3f8d50058264d5a220e573
HusseinALi97 <venom.blackdeser@gmail.com>
2023-06-17 11:50:46 +0300

    js2

--- A6/Js/main.js
+++ A6/Js/main.js
@@ -34,8 +34,7 @@ window.addEventListener('load', function () {
   var spinner = document.getElementById('spinner');
   
   setTimeout(function () {
-      spinner.style.display = 'none';
-  }, 1500); 
+      spinner.style.display = 'none'; // Hide the spinner after the specified duration
+  }, 1500); // Adjust the duration as needed
 });
-
 
