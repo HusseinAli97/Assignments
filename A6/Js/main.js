2b3bdfdb8e2a4bb9645ae375f056742bcd30249a
HusseinALi97 <venom.blackdeser@gmail.com>
2023-06-17 06:59:18 +0300

    SOME FORMATING

--- A6/Js/main.js
+++ A6/Js/main.js
@@ -1,27 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('.counter').text("0");
 
   var waypoint = new Waypoint({
     element: $('.py-5')[0],
-    handler: function(direction) {
+    handler: function (direction) {
       if (direction === 'down') {
-        $('.counter').each(function() {
+        $('.counter').each(function () {
           var $this = $(this),
-          countTo = Math.floor(Math.random() * 1000);
+            countTo = Math.floor(Math.random() * 1000);
 
-          $({ countNum: $this.text()}).animate({
+          $({ countNum: $this.text() }).animate({
             countNum: countTo
           },
-          {
-            duration: 2000,
-            easing:'linear',
-            step: function() {
-              $this.text(Math.floor(this.countNum));
-            },
-            complete: function() {
-              $this.text(this.countNum);
-            }
-          });
+            {
+              duration: 2000,
+              easing: 'linear',
+              step: function () {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function () {
+                $this.text(this.countNum);
+              }
+            });
         });
         this.destroy();
       }
